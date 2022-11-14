49d971f10846dd2eedd06b872d302d3d71623ffc
ZadeRohan <rohanramdaszade@p2fsemi.in>
2022-11-14 19:11:09 +0530

    enhanced def and timing parser and added test for def and timing

--- src/parsers/def_parser.py
+++ src/parsers/def_parser.py
@@ -1,74 +1,90 @@
 import json
-import sys
 
-main_dict = {}
-index_end = 0                           # it was throwing error in py file so initialized it here
-#opened file in read mode
-with open(sys.argv[1]) as f:
-    lines = f.readlines()
-    # iterating over each line
-    i=0 
-    for line in lines:
-        # getting line containing die area
-        if line.startswith('DIEAREA'):
-            line1 = line.split(" ")
-            main_dict['DIEAREA']={'X1':float(line1[2]),
-                                    'Y1':float(line1[3]),
-                                    'X2':float(line1[6]),
-                                    'Y2':float(line1[7])}
-        # getting line containg COMPONENTS and getting start and end index
-        elif line.startswith('COMPONENTS'):
-            line1 = line.split(" ")
-            index_start = lines.index(line)
-            index_end = lines.index('END COMPONENTS\n')
-            main_dict['Number of components']=int(line1[1])
-        
-        # lines containing component part with start and end index
-        
-        elif lines.index(line)<index_end and lines.index(line)>index_start:
-            line3 = line.split("\t")
-            line3 = list(filter(None, line3))   #filtering null values
-            print("line3:",line3)
-            # before we splitted on \t now spliting on space for getting X, Y and Direction values 
-            try:
-                line3_1 = line3[2].split(" ")
-            except:
+def parse_def(input, output):
+    main_dict = {}
+    index_end = 0 
+    components = []                          # it was throwing error in py file so initialized it here
+    #opened file in read mode
+    with open(input) as f:
+        lines = f.readlines()
+        # iterating over each line
+        i=0 
+        for line in lines:
+            if line.startswith('UNITS DISTANCE'):
+                line1 = line.split(" ")
+                
+                main_dict['unit_in_micron']=float(line1[3])
+
+                # converting co-oradinate values to mm
+                mul_unit = main_dict['unit_in_micron']*(10**-6)
+                
+            
+
+
+            # getting line containing die area
+            elif line.startswith('DIEAREA'):
+                line1 = line.split(" ")
+                main_dict['diearea']={'x1':float(line1[2])*mul_unit,
+                                        'y1':float(line1[3])*mul_unit,
+                                        'x2':float(line1[6])*mul_unit,
+                                        'y2':float(line1[7])*mul_unit}
+            # getting line containg COMPONENTS and getting start and end index
+            elif line.startswith('COMPONENTS'):
+                line1 = line.split(" ")
+                index_start = lines.index(line)
+                index_end = lines.index('END COMPONENTS\n')
+                main_dict['number_of_components']=int(line1[1])
+            
+            # lines containing component part with start and end index
+            
+            elif lines.index(line)<index_end and lines.index(line)>index_start:
+                line3 = line.split("\t")
+                line3 = list(filter(None, line3))   #filtering null values
+              
+                # before we splitted on \t now spliting on space for getting X, Y and Direction values 
                 try:
-                    line3_1 = line3[1].split(" ")
+                    line3_1 = line3[2].split(" ")
                 except:
-                    line3_1 = line3[0].split(" ")
+                    try:
+                        line3_1 = line3[1].split(" ")
+                    except:
+                        line3_1 = line3[0].split(" ")
 
-            try:
-                line3_2 = line3[1].strip("-").split(" ") #splitting some unsplitted lines and extracting instance name
-                line3_2 = list(filter(None, line3_2))
-            except:
-                line3_2 = line3[0].strip("-").split(" ")
-                line3_2 = list(filter(None, line3_2))
-            print("line3_2:", line3_2)
+                try:
+                    line3_2 = line3[1].strip("-").split(" ") #splitting some unsplitted lines and extracting instance name
+                    line3_2 = list(filter(None, line3_2))
+                except:
+                    line3_2 = line3[0].strip("-").split(" ")
+                    line3_2 = list(filter(None, line3_2))
+                
 
-            def parse_components(word):
-                if word in line3_1:
-                    main_dict[f'Comp {i}']={
-                                'Cell_name':line3[0].strip('- ').split(" ")[0],
-                                'Instance_name':line3_2[0],
-                                'X':float(line3_1[line3_1.index(word)+2]),
-                                'Y':float(line3_1[line3_1.index(word)+3]),
-                                'Direction':line3_1[line3_1.index(word)+5]                           
-                                }
+                def parse_components(word):
+                    if word in line3_1:
+                        components.append({
+                                    'Comp_id':i,
+                                    'cell_name':line3[0].strip('- ').split(" ")[0],
+                                    'instance_name':line3_2[0],
+                                    'x':float(line3_1[line3_1.index(word)+2])*mul_unit,
+                                    'y':float(line3_1[line3_1.index(word)+3])*mul_unit,
+                                    'direction':line3_1[line3_1.index(word)+5]                           
+                                    })
             
-            
-            parse_components('COVER')
-            parse_components('FIXED')
-            parse_components('PLACED')
-            i+=1
+                
+                
+                parse_components('COVER')
+                parse_components('FIXED')
+                parse_components('PLACED')
+                i+=1
+    main_dict['components']=components
+    main_json = json.dumps(main_dict, indent=4)
 
-main_json = json.dumps(main_dict, indent=4)
-print(main_json)
+    print(len(main_dict['components']))
+    print(main_dict['number_of_components'])
+    if len(main_dict)!=main_dict['number_of_components']:
+        print('There are {} missing components'.format(main_dict['number_of_components']-len(main_dict['components'])))
+        
+    with open(output, 'w') as output1:
+        json.dump(main_dict, output1, indent=4)
+    return output
 
-print(len(main_dict))
-print(main_dict['Number of components'])
-if len(main_dict)!=main_dict['Number of components']:
-    print('There are {} missing components'.format(main_dict['Number of components']-len(main_dict)))
-    
-with open(sys.argv[2], 'w') as output:
-    json.dump(main_dict, output, indent=4)
+parse_def('dd1.def', 'def_out.json')
--- src/parsers/timing_parser.py
+++ src/parsers/timing_parser.py
@@ -1,120 +1,127 @@
 import json
 import sys
 # defining keys
-Start=[]
-Start_clk_by=[]
-End=[]
-End_clk_by=[]
-required_time=[]
-Path_Group=[]
-Path_Type=[]
-slack=[]
-points = []
-data={}
-point_index=[]
-slack_index=[]
 
-with open(sys.argv[1],'r') as d:
-    lines=d.readlines()
+def parse_timing(input, output):
+    Start=[]
+    Start_clk_by=[]
+    End=[]
+    End_clk_by=[]
+    required_time=[]
+    Path_Group=[]
+    Path_Type=[]
+    slack=[]
+    points = []
+    data={}
+    point_index=[]
+    slack_index=[]
 
-    # i=0
-    for i, line in enumerate(lines):
-        if line.strip().startswith('Startpoint:'):
-            c=line.split(':')[1]
-            c=c.split('(')
-            d=c[1].strip('),\n').split('by')
-            Start.append(c[0])
-            Start_clk_by.append(d[1])
+    with open(input,'r') as d:
+        lines=d.readlines()
 
-        elif line.strip().startswith('Endpoint:'):
-            c=line.split(':')[1]
-            c=c.split('(')
-            d=c[1].strip('),\n').split('by')
-            End.append(c[0])
-            End_clk_by.append(d[1])
+        # i=0
+        for i, line in enumerate(lines):
+            if line.strip().startswith('Startpoint:'):
+                c=line.split(':')[1]
+                c=c.split('(')
+                d=c[1].strip('),\n').split('by')
+                Start.append(c[0])
+                Start_clk_by.append(d[1])
 
-        elif line.strip().startswith('Path Group:'):
-            Path_Group.append(line.strip('\n').split(':')[1])
+            elif line.strip().startswith('Endpoint:'):
+                c=line.split(':')[1]
+                c=c.split('(')
+                d=c[1].strip('),\n').split('by')
+                End.append(c[0])
+                End_clk_by.append(d[1])
 
-        elif line.strip().startswith('Path Type:'):
-            Path_Type.append(line.strip('\n').split(':')[1]) 
+            elif line.strip().startswith('Path Group:'):
+                Path_Group.append(line.strip('\n').split(':')[1])
 
-        elif line.strip().startswith('Point'):
-            point_index.append(i)
+            elif line.strip().startswith('Path Type:'):
+                Path_Type.append(line.strip('\n').split(':')[1]) 
 
-        elif line.strip().startswith('data required time'):
-            c=line.strip('\n,').split('  ')
-            c=list(filter(None,c))
-            required_time.append(float(c[1])) 
+            elif line.strip().startswith('Point'):
+                point_index.append(i)
 
-        elif line.strip().startswith('slack'):
-            c=line.strip('\n').split(' ')
-            c=list(filter(None,c))
-            slack.append(c[2])
-            slack_index.append(i)
-        # i+=1
+            elif line.strip().startswith('data required time'):
+                c=line.strip('\n,').split('  ')
+                c=list(filter(None,c))
+                required_time.append(float(c[1])) 
 
-    #defining main points list where all paths are added 
-    points = []  
-    for j in range(len(point_index)):
-        points_sub = []
-        for count, line in enumerate(lines[point_index[j]:(slack_index[j]-14)]):
-            # we are accessing 3 lines at a time so skipping 2 indices    
-            if count%3!=0:
-                pass
-            else:
-                try:
-                    # accessing 3 lines at a time input, output and net lines 
-                    li_in = lines[point_index[j]+count+5].split()
-                    li_out = lines[point_index[j]+count+6].split()
-                    li_net = lines[point_index[j]+count+7].split()
+            elif line.strip().startswith('slack'):
+                c=line.strip('\n').split(' ')
+                c=list(filter(None,c))
+                slack.append(c[2])
+                slack_index.append(i)
+            # i+=1
+
+        #defining main points list where all paths are added 
+        points = []  
+        for j in range(len(point_index)):
+            points_sub = []
+            for count, line in enumerate(lines[point_index[j]:(slack_index[j]-14)]):
+                # we are accessing 3 lines at a time so skipping 2 indices    
+                if count%3!=0:
+                    pass
+                else:
+                    try:
+                        # accessing 3 lines at a time input, output and net lines 
+                        li_in = lines[point_index[j]+count+5].split()
+                        li_out = lines[point_index[j]+count+6].split()
+                        li_net = lines[point_index[j]+count+7].split()
+                        print(li_in)
+                        dict1={
+                            "point":int(count/3),
+                            "instance_name" : li_in[0],
+                            "cell_name" : li_in[1].strip("()"),
+                            "net" : li_net[0],
+                            "in_trans" : float(li_in[2]),
+                            "out_trans" : float(li_out[2]),
+                            "delay_increment" : float(li_out[3])-float(li_in[3]),
+                            "net_fanout" : float(li_net[2]),
+                            "net_cap" : float(li_net[3]),
+                            "delay_point" : float(li_out[4]),
+                            }
+                        points_sub.append(dict1)
+                    except:
+                        li_in = lines[point_index[j]+count+5].split()
+                        dict1={
+                            "point":int(count/3),
+                            "instance_name" : li_in[0],
+                            "cell_name" : li_in[1].strip("()"),
+                            "net" : "-",
+                            "in_trans" : float(li_in[2]),
+                            "out_trans" : "-",
+                            "delay_increment" : "-",
+                            "net_fanout" : "-",
+                            "net_cap" : "-",
+                            "delay_point" : "-",
+                            }
+                        points_sub.append(dict1)
+                        break
+                        
+            points.append(points_sub)
                     
-                    dict1={
-                        "Point":int(count/3),
-                        "Instance_name" : li_in[0],
-                        "Cell_name" : li_in[1].strip("()"),
-                        "Net" : li_net[0],
-                        "In_trance" : float(li_in[2]),
-                        "out_trance" : float(li_out[2]),
-                        "Delay_increment" : float(li_out[3])-float(li_in[3]),
-                        "Net_fanout" : float(li_net[2]),
-                        "Net_cap" : float(li_net[3]),
-                        "Delay_point" : float(li_out[4]),
-                        }
-                    points_sub.append(dict1)
-                except:
-                    li_in = lines[point_index[j]+count+5].split()
-                    dict1={
-                        "Point":int(count/3),
-                        "Instance_name" : li_in[0],
-                        "Cell_name" : li_in[1].strip("()"),
-                        "Net" : "-",
-                        "In_trance" : float(li_in[2]),
-                        "out_trance" : "-",
-                        "Delay_increment" : "-",
-                        "Net_fanout" : "-",
-                        "Net_cap" : "-",
-                        "Delay_point" : "-",
-                        }
-                    points_sub.append(dict1)
-                    break
-                       
-        points.append(points_sub)
                 
-            
-with open(sys.argv[2],"w") as outfile:  
-             
-    d=[{
-                'Path':i,
-                "Start":Start[i],
-                "Start_clk_by":Start_clk_by[i].strip(" )\t"),
-                "End":End[i],
-                "End_clk_by":End_clk_by[i].strip(" )\t"),
-                "Data required time":required_time[i+1],
-                "Path Group":Path_Group[i].strip(" \t"),
-                "Path Type":Path_Type[i].strip(" )\t"),
-                "Slack":float(slack[i].strip("\t")),
-                "point":points[i]
-                } for i in range(len(End))]
+    with open(output,"w") as outfile:  
+                
+        d=[{
+                    'path':i,
+                    "start":Start[i],
+                    "start_clk_by":Start_clk_by[i].strip(" )\t"),
+                    "end":End[i],
+                    "end_clk_by":End_clk_by[i].strip(" )\t"),
+                    "data_required_time":required_time[i+1],
+                    "path_group":Path_Group[i].strip(" \t"),
+                    "path_type":Path_Type[i].strip(" )\t"),
+                    "slack":float(slack[i].strip("\t")),
+                    "points":points[i]
+                    } for i in range(len(End))]
 
-    json.dump(d,outfile,indent=4)
+        json.dump(d,outfile,indent=4)
+    return output
+    
+
+out= parse_timing('setup.txt', 'timing_out.json')
+print(out, "#########")
