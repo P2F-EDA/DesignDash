5fa474a2183f4c74c73d14f8e5a4c346ffeb6cc7
z-rohan <zrohan2016@gmail.com>
2022-11-21 09:19:20 +0530

    add two input files for parser(dd1.def and fulladder_V0.1.def)
    modify parser to extract both the files
    take into considaration that diearea can have 4 coordinates
    add new function to parse UNPLACED components

--- src/parsers/def_parser.py
+++ src/parsers/def_parser.py
@@ -10,24 +10,39 @@ def parse_def(input, output):
         # iterating over each line
         i=0 
         for line in lines:
-            if line.startswith('UNITS DISTANCE'):
+            if line.startswith('DESIGN'):
+                line = line.split()
+                main_dict['design'] = line[1]
+
+            elif line.startswith('UNITS DISTANCE'):
                 line1 = line.split(" ")
-                
                 main_dict['unit_in_micron']=float(line1[3])
-
-                # converting co-oradinate values to mm
-                mul_unit = main_dict['unit_in_micron']*(10**-6)
+                # converting co-oradinate values
+                mul_unit = main_dict['unit_in_micron']*(10**-3)
                 
-            
-
-
             # getting line containing die area
             elif line.startswith('DIEAREA'):
                 line1 = line.split(" ")
-                main_dict['diearea']={'x1':float(line1[2])*mul_unit,
+                # print(line1)
+                if len(line1)<12:
+                    main_dict['diearea'] = {
+                                        'x1':float(line1[2])*mul_unit,
                                         'y1':float(line1[3])*mul_unit,
                                         'x2':float(line1[6])*mul_unit,
-                                        'y2':float(line1[7])*mul_unit}
+                                        'y2':float(line1[7])*mul_unit
+                                        }
+                else:
+                    main_dict['diearea'] = {
+                                            'x1':float(line1[2])*mul_unit,
+                                            'y1':float(line1[3])*mul_unit,
+                                            'x2':float(line1[6])*mul_unit,
+                                            'y2':float(line1[7])*mul_unit,
+                                            'x3':float(line1[10])*mul_unit,
+                                            'y3':float(line1[11])*mul_unit,
+                                            'x4':float(line1[14])*mul_unit,
+                                            'y4':float(line1[15])*mul_unit
+                                            }
+                    
             # getting line containg COMPONENTS and getting start and end index
             elif line.startswith('COMPONENTS'):
                 line1 = line.split(" ")
@@ -38,42 +53,34 @@ def parse_def(input, output):
             # lines containing component part with start and end index
             
             elif lines.index(line)<index_end and lines.index(line)>index_start:
-                line3 = line.split("\t")
-                line3 = list(filter(None, line3))   #filtering null values
-              
-                # before we splitted on \t now spliting on space for getting X, Y and Direction values 
-                try:
-                    line3_1 = line3[2].split(" ")
-                except:
-                    try:
-                        line3_1 = line3[1].split(" ")
-                    except:
-                        line3_1 = line3[0].split(" ")
-
-                try:
-                    line3_2 = line3[1].strip("-").split(" ") #splitting some unsplitted lines and extracting instance name
-                    line3_2 = list(filter(None, line3_2))
-                except:
-                    line3_2 = line3[0].strip("-").split(" ")
-                    line3_2 = list(filter(None, line3_2))
+                line3 = line.split()
                 
-
                 def parse_components(word):
-                    if word in line3_1:
+                    if word in line3:
                         components.append({
                                     'Comp_id':i,
-                                    'cell_name':line3[0].strip('- ').split(" ")[0],
-                                    'instance_name':line3_2[0],
-                                    'x':float(line3_1[line3_1.index(word)+2])*mul_unit,
-                                    'y':float(line3_1[line3_1.index(word)+3])*mul_unit,
-                                    'direction':line3_1[line3_1.index(word)+5]                           
+                                    'ref_name':line3[2],
+                                    'instance_name':line3[1],
+                                    'x':float(line3[line3.index(word)+2])*mul_unit,
+                                    'y':float(line3[line3.index(word)+3])*mul_unit,
+                                    'direction':line3[line3.index(word)+5]                           
                                     })
-            
+                def parse_components1(word):
+                    if word in line3:
+                        components.append({
+                                    'Comp_id':i,
+                                    'ref_name':line3[2],
+                                    'instance_name':line3[1],
+                                    'x':'-',
+                                    'y':'-',
+                                    'direction':'-'                           
+                                    })
                 
                 
                 parse_components('COVER')
                 parse_components('FIXED')
                 parse_components('PLACED')
+                parse_components1('UNPLACED')
                 i+=1
     main_dict['components']=components
     main_json = json.dumps(main_dict, indent=4)
@@ -87,4 +94,4 @@ def parse_def(input, output):
         json.dump(main_dict, output1, indent=4)
     return output
 
-parse_def('dd1.def', 'def_out.json')
+parse_def('fulladder_V0.1.def', 'def_out.json')
